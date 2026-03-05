8360f63c180a3d43bf0227bc60bc3cc4c87245f2
rzndkprtm <rzndkprtm@gmail.com>
2026-03-05 10:21:12 +0700

    RAP

--- Scripts/WebOrders/Venetian.js
+++ Scripts/WebOrders/Venetian.js
@@ -869,7 +869,7 @@ function controlForm(status, isEditItem, isCopyItem) {
         if (inputElement) {
             if (isCopyItem) {
                 inputElement.disabled = (id === "blindtype");
-            } else if (isEditItem && (id === "qty" || id === "blindtype")) {
+            } else if (isEditItem && (id === "qty")) {
                 inputElement.disabled = true;
             } else {
                 inputElement.disabled = status;
