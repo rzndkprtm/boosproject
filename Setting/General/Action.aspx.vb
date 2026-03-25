cb938790fe76f6f2f8cbaaa867c9c3d0805308f9
rzndkprtm <rzndkprtm@gmail.com>
2026-03-25 09:06:52 +0700

    RAP

--- Setting/General/Action.aspx.vb
+++ Setting/General/Action.aspx.vb
@@ -191,7 +191,7 @@ Partial Class Setting_General_Action
         Try
             Dim search As String = String.Empty
             If Not searchText = "" Then
-                search = " WHERE Actions.Id LIKE '%" & searchText.Trim() & "%' OR Actions.Page LIKE '%" & searchText.Trim() & "%' OR Actions.Action LIKE '%" & searchText.Trim() & "%' OR Actions.Description LIKE '%" & searchText.Trim() & "%' OR LoginRoles.Name LIKE '%" & searchText.Trim() & "%' OR LoginLevels.Name LIKE '%" & searchText.Trim() & "%'"
+                search = " WHERE Actions.Id LIKE '%" & searchText.Trim() & "%' OR Actions.Page LIKE '%" & searchText.Trim() & "%' OR LoginRoles.Name LIKE '%" & searchText.Trim() & "%' OR LoginLevels.Name LIKE '%" & searchText.Trim() & "%'"
             End If
             Dim thisString As String = String.Format("SELECT Actions.*, LoginRoles.Name AS RoleName, LoginLevels.Name AS LevelName, CASE WHEN Actions.Active=1 THEN 'Yes' WHEN Actions.Active=0 THEN 'No' ELSE 'Error' END AS DataActive FROM Actions LEFT JOIN LoginRoles ON Actions.RoleId=LoginRoles.Id LEFT JOIN LoginLevels ON Actions.LevelId=LoginLevels.Id {0} ORDER BY Actions.RoleId, Actions.LevelId, Actions.Page ASC", search)
 
