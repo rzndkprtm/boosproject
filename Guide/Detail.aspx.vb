6567887baff142d5ede3a26c1483446a11d281ec
rzndkprtm <rzndkprtm@gmail.com>
2026-03-29 14:50:28 +0700

    RAP

--- Guide/Detail.aspx.vb
+++ Guide/Detail.aspx.vb
@@ -49,6 +49,12 @@ Partial Class Guide_Detail
             End If
         Catch ex As Exception
             MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+                If Session("RoleName") = "Customer" Then
+                    MessageError(True, "PLEASE CONTACT YOUR CUSTOMER SERVICE !")
+                End If
+            End If
         End Try
     End Sub
 
