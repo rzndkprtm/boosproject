cabad13ff9307a0035662791d9ecc5dc9aef5f04
rzndkprtm <rzndkprtm@gmail.com>
2026-04-30 08:02:43 +0700

    RAP

--- Setting/Specification/Fabric/Jakarta.aspx.vb
+++ Setting/Specification/Fabric/Jakarta.aspx.vb
@@ -1,5 +1,84 @@
-﻿
-Partial Class Setting_Specification_Fabric_Jakarta
-    Inherits System.Web.UI.Page
+﻿Imports System.Data
+Imports System.Data.SqlClient
 
+Partial Class Setting_Specification_Fabric_Jakarta
+    Inherits Page
+
+    Dim settingClass As New SettingClass
+
+    Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+    Dim dataLog As Object() = Nothing
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim pageAccess As Boolean = PageAction("Load")
+        If pageAccess = False Then
+            Response.Redirect("~/setting/specification/fabric", False)
+            Exit Sub
+        End If
+
+        If Not IsPostBack Then
+            MessageError(False, String.Empty)
+
+            txtSearch.Text = Session("SearchFabricJakarta")
+
+            BindData(txtSearch.Text)
+        End If
+    End Sub
+
+    Protected Sub btnAdd_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnSearch_Click(sender As Object, e As EventArgs)
+        MessageError(False, String.Empty)
+        BindData(txtSearch.Text)
+    End Sub
+
+    Protected Sub gvList_PageIndexChanging(sender As Object, e As GridViewPageEventArgs)
+
+    End Sub
+
+    Protected Sub gvList_RowCommand(sender As Object, e As GridViewCommandEventArgs)
+
+    End Sub
+
+    Protected Sub BindData(searchText As String)
+        Session("SearchFabricJakarta") = String.Empty
+        Try
+            Dim stringSearch As String = String.Empty
+            If Not searchText = "" Then
+                stringSearch = "WHERE Fabrics.Name LIKE '%" & searchText & "%'"
+            End If
+
+            Dim thisString As String = String.Format("SELECT FabricGroupLocals.*, Fabrics.Name AS FabricName FROM FabricGroupLocals LEFT JOIN Fabrics ON FabricGroupLocals.Id=Fabrics.Id {0}", stringSearch)
+            gvList.DataSource = settingClass.GetDataTable(thisString)
+            gvList.DataBind()
+            gvList.Columns(1).Visible = PageAction("Visible ID")
+
+            btnAdd.Visible = PageAction("Add")
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+            End If
+        End Try
+    End Sub
+
+    Protected Sub MessageError(visible As Boolean, message As String)
+        divError.Visible = visible : msgError.InnerText = message
+    End Sub
+
+    Protected Function PageAction(action As String) As Boolean
+        Try
+            Dim roleId As String = Session("RoleId").ToString()
+            Dim levelId As String = Session("LevelId").ToString()
+            Dim actionClass As New ActionClass
+
+            Return actionClass.GetActionAccess(roleId, levelId, Page.Title, action)
+        Catch ex As Exception
+            Response.Redirect("~/account/login", False)
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+            Return False
+        End Try
+    End Function
 End Class
