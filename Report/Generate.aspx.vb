5f81d9bb83d52bcbbab31b9597ab7caaf246ea9c
rzndkprtm <rzndkprtm@gmail.com>
2026-05-21 11:29:11 +0700

    RAP

--- Report/Generate.aspx.vb
+++ Report/Generate.aspx.vb
@@ -13,6 +13,47 @@ Partial Class Report_Generate
             Response.Redirect("~/report/", False)
             Exit Sub
         End If
+
+        If Not IsPostBack Then
+            MessageError(False, String.Empty)
+            BindCompany()
+        End If
+    End Sub
+
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub btnCancel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub BindCompany()
+        ddlCompany.Items.Clear() : ddlCompany.Enabled = True
+        Try
+            ddlCompany.DataSource = reportClass.GetDataTable("SELECT * FROM Companys WHERE Active=1 ORDER BY Name ASC")
+            ddlCompany.DataTextField = "Alias"
+            ddlCompany.DataValueField = "Id"
+            ddlCompany.DataBind()
+
+            If ddlCompany.Items.Count > 0 Then
+                ddlCompany.Items.Insert(0, New ListItem("ALL", "ALL"))
+            End If
+
+            If Session("RoleName") = "Sales" Then
+                ddlCompany.SelectedValue = Session("CompanyId").ToString()
+                ddlCompany.Enabled = False
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+            End If
+        End Try
+    End Sub
+
+    Protected Sub MessageError(visible As Boolean, message As String)
+        divError.Visible = visible : msgError.InnerText = message
     End Sub
 
     Protected Function PageAction(action As String) As Boolean
