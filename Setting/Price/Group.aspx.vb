ff473786b0beb3c522cb545aa4327878a98a6a3a
rzndkprtm <rzndkprtm@gmail.com>
2026-06-15 16:12:36 +0700

    RAP

--- Setting/Price/Group.aspx.vb
+++ Setting/Price/Group.aspx.vb
@@ -5,7 +5,6 @@ Partial Class Setting_Price_Group
     Inherits Page
 
     Dim settingClass As New SettingClass
-
     Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
     Dim dataLog As Object() = Nothing
 
