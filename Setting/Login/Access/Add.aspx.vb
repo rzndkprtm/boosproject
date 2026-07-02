d1391adae2c90ed868b7efa874660fc43172aea6
rzndkprtm <rzndkprtm@gmail.com>
2026-07-02 21:19:48 +0700

    RAP

---
+++ Setting/Login/Access/Add.aspx.vb
@@ -0,0 +1,168 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Partial Class Setting_Login_Access_Add
+    Inherits Page
+
+    Dim settingClass As New SettingClass
+    Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim pageAccess As Boolean = LoginAccess("Load")
+        If pageAccess = False Then
+            Response.Redirect("~/setting/login/access", False)
+            Exit Sub
+        End If
+
+        If Not IsPostBack Then
+            MessageError(False, String.Empty)
+            BindRole()
+            BindLevel()
+            BindPage()
+        End If
+    End Sub
+
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs)
+        MessageError(False, String.Empty)
+        Try
+            If ddlRoleId.SelectedValue = "" Then
+                MessageError(True, "ROLE NAME IS REQUIRED !")
+                Exit Sub
+            End If
+            If ddlLevelId.SelectedValue = "" Then
+                MessageError(True, "LEVEL ACCESS IS REQUIRED !")
+                Exit Sub
+            End If
+            If ddlPage.SelectedValue = "" Then
+                MessageError(True, "PAGE IS REQUIRED !")
+                Exit Sub
+            End If
+            If txtAction.Text = "" Then
+                MessageError(True, "ACTION IS REQUIRED !")
+                Exit Sub
+            End If
+            If msgError.InnerText = "" Then
+                Dim descText As String = txtDescription.Text.Replace(vbCrLf, "").Replace(vbCr, "").Replace(vbLf, "")
+
+                Using thisConn As New SqlConnection(myConn)
+                    Using thisCmd As SqlCommand = New SqlCommand("INSERT INTO LoginAccess VALUES (NEWID(), @RoleId, @LevelId, @Page, @Action, @Description, @Active)", thisConn)
+                        thisCmd.Parameters.AddWithValue("@RoleId", ddlRoleId.SelectedValue)
+                        thisCmd.Parameters.AddWithValue("@LevelId", ddlLevelId.SelectedValue)
+                        thisCmd.Parameters.AddWithValue("@Page", ddlPage.SelectedValue)
+                        thisCmd.Parameters.AddWithValue("@Action", txtAction.Text.Trim())
+                        thisCmd.Parameters.AddWithValue("@Description", descText)
+                        thisCmd.Parameters.AddWithValue("@Active", ddlActive.SelectedValue)
+                        thisConn.Open()
+                        thisCmd.ExecuteNonQuery()
+                    End Using
+                End Using
+
+                Response.Redirect("~/setting/login/access", False)
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+        End Try
+    End Sub
+
+    Protected Sub btnCancel_Click(sender As Object, e As EventArgs)
+        Response.Redirect("~/setting/login/access", False)
+    End Sub
+
+    Protected Sub BindRole()
+        ddlRoleId.Items.Clear()
+        Try
+            ddlRoleId.DataSource = settingClass.GetDataTable("SELECT * FROM LoginRoles ORDER BY Name ASC")
+            ddlRoleId.DataTextField = "Name"
+            ddlRoleId.DataValueField = "Id"
+            ddlRoleId.DataBind()
+
+            If ddlRoleId.Items.Count > 1 Then
+                ddlRoleId.Items.Insert(0, New ListItem("", ""))
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+        End Try
+    End Sub
+
+    Protected Sub BindLevel()
+        ddlLevelId.Items.Clear()
+        Try
+            ddlLevelId.DataSource = settingClass.GetDataTable("SELECT * FROM LoginLevels ORDER BY Name ASC")
+            ddlLevelId.DataTextField = "Name"
+            ddlLevelId.DataValueField = "Id"
+            ddlLevelId.DataBind()
+
+            If ddlLevelId.Items.Count > 1 Then
+                ddlLevelId.Items.Insert(0, New ListItem("", ""))
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+        End Try
+    End Sub
+
+    Protected Sub BindPage()
+        Try
+            ddlPage.Items.Clear()
+
+            Dim rootPath As String = Server.MapPath("~/")
+
+            Dim ignoreFolders As New List(Of String) From {"\bin\", "\obj\", "\App_Data\", "\App_Code\", "\Scripts\", "\Content\", "\Images\", "\fonts\", "\packages\", "\Properties\"}
+
+            Dim regex As New Regex("Title\s*=\s*""([^""]*)""", RegexOptions.IgnoreCase)
+
+            Dim pageList As New List(Of ListItem)
+
+            For Each file As String In Directory.EnumerateFiles(rootPath, "*.aspx", SearchOption.AllDirectories)
+                Try
+                    Dim skip As Boolean = False
+
+                    For Each folder As String In ignoreFolders
+                        If file.IndexOf(folder, StringComparison.OrdinalIgnoreCase) >= 0 Then
+                            skip = True
+                            Exit For
+                        End If
+                    Next
+
+                    If skip Then Continue For
+
+                    Dim content As String = IO.File.ReadAllText(file)
+                    Dim match As Match = regex.Match(content)
+
+                    Dim title As String
+
+                    If match.Success Then
+                        title = match.Groups(1).Value.Trim()
+                    Else
+                        title = Path.GetFileNameWithoutExtension(file)
+                    End If
+
+                    pageList.Add(New ListItem(title))
+                Catch
+                    Continue For
+                End Try
+            Next
+            pageList = pageList.OrderBy(Function(x) x.Text).ToList()
+            ddlPage.Items.AddRange(pageList.ToArray())
+        Catch ex As Exception
+            MessageError(True, ex.Message)
+        End Try
+    End Sub
+
+    Protected Sub MessageError(visible As Boolean, message As String)
+        divError.Visible = visible : msgError.InnerText = message
+    End Sub
+
+    Protected Function LoginAccess(action As String) As Boolean
+        Try
+            Dim roleId As String = Session("RoleId").ToString()
+            Dim levelId As String = Session("LevelId").ToString()
+            Dim accessClass As New AccessClass
+
+            Return accessClass.GetLoginAccess(roleId, levelId, Page.Title, action)
+        Catch ex As Exception
+            Response.Redirect("~/account/login", False)
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+            Return False
+        End Try
+    End Function
+End Class
---
+++ Setting/Login/Access/Default.aspx.vb
@@ -0,0 +1,141 @@
+﻿Imports System.Data.SqlClient
+
+Partial Class Setting_Login_Access_Default
+    Inherits Page
+
+    Dim settingClass As New SettingClass
+    Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim pageAccess As Boolean = LoginAccess("Load")
+        If pageAccess = False Then
+            Response.Redirect("~/setting/login", False)
+            Exit Sub
+        End If
+
+        If Not IsPostBack Then
+            MessageError(False, String.Empty)
+            txtSearch.Text = Session("SearchLoginAccess")
+            BindData(txtSearch.Text)
+        End If
+    End Sub
+
+    Protected Sub btnAdd_Click(sender As Object, e As EventArgs)
+        Session("SearchLoginAccess") = txtSearch.Text
+        Response.Redirect("~/setting/login/access/add", False)
+    End Sub
+
+    Protected Sub btnSearch_Click(sender As Object, e As EventArgs)
+        gvList.PageIndex = 0
+
+        MessageError(False, String.Empty)
+        BindData(txtSearch.Text)
+
+        Session("SearchLoginAccess") = txtSearch.Text
+    End Sub
+
+    Protected Sub rptPager_ItemCommand(sender As Object, e As RepeaterCommandEventArgs)
+        If e.CommandName = "Page" Then
+            gvList.PageIndex = Convert.ToInt32(e.CommandArgument)
+            BindData(txtSearch.Text)
+        End If
+    End Sub
+
+    Protected Sub gvList_DataBound(sender As Object, e As EventArgs)
+        BuildPager()
+    End Sub
+
+    Protected Sub gvList_PageIndexChanging(sender As Object, e As GridViewPageEventArgs)
+        gvList.PageIndex = e.NewPageIndex
+        MessageError(False, String.Empty)
+        BindData(txtSearch.Text)
+    End Sub
+
+    Protected Sub btnDelete_Click(sender As Object, e As EventArgs)
+        MessageError(False, String.Empty)
+        Try
+            Dim thisId As String = txtDeleteId.Text
+
+            Using thisConn As New SqlConnection(myConn)
+                Using thisCmd As SqlCommand = New SqlCommand("DELETE FROM LoginAccess WHERE Id=@Id", thisConn)
+                    thisCmd.Parameters.AddWithValue("@Id", thisId)
+                    thisConn.Open()
+                    thisCmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            Session("SearchLoginAccess") = txtSearch.Text
+            Response.Redirect("~/setting/login/access", False)
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+        End Try
+    End Sub
+
+    Protected Sub BindData(searchText As String)
+        Try
+            Dim params As New List(Of SqlParameter) From {
+                New SqlParameter("@SearchText", If(String.IsNullOrEmpty(searchText), "", searchText.Trim()))
+            }
+            gvList.DataSource = settingClass.GetDataTableSP("sp_LoginAccess_List", params)
+            gvList.DataBind()
+
+            btnAdd.Visible = LoginAccess("Add")
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+        End Try
+    End Sub
+
+    Protected Sub BuildPager()
+        Try
+            If gvList.PageCount <= 1 Then
+                navPager.Visible = False
+                Return
+            End If
+
+            navPager.Visible = True
+
+            Dim currentPage As Integer = gvList.PageIndex
+            Dim totalPages As Integer = gvList.PageCount
+
+            Dim pages As New List(Of Object)
+
+            If currentPage > 0 Then
+                pages.Add(New With {.Text = "Previous", .PageIndex = currentPage - 1, .CssClass = ""})
+            End If
+
+            Dim startPage As Integer = Math.Max(0, currentPage - 2)
+            Dim endPage As Integer = Math.Min(totalPages - 1, currentPage + 2)
+
+            For i As Integer = startPage To endPage
+                pages.Add(New With {.Text = (i + 1).ToString(), .PageIndex = i, .CssClass = If(i = currentPage, "active", "")})
+            Next
+
+            If currentPage < totalPages - 1 Then
+                pages.Add(New With {.Text = "Next", .PageIndex = currentPage + 1, .CssClass = ""})
+            End If
+
+            rptPager.DataSource = pages
+            rptPager.DataBind()
+        Catch ex As Exception
+            navPager.Visible = False
+        End Try
+    End Sub
+
+    Protected Sub MessageError(visible As Boolean, message As String)
+        divError.Visible = visible : msgError.InnerText = message
+    End Sub
+
+    Protected Function LoginAccess(action As String) As Boolean
+        Try
+            Dim roleId As String = Session("RoleId").ToString()
+            Dim levelId As String = Session("LevelId").ToString()
+            Dim accessClass As New AccessClass
+
+            Return accessClass.GetLoginAccess(roleId, levelId, Page.Title, action)
+        Catch ex As Exception
+            Response.Redirect("~/account/login", False)
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+            Return False
+        End Try
+    End Function
+End Class
