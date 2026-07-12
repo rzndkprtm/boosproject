c9c61b7a383a5487f70a9a0cd8e6965051cddeb4
rzndkprtm <rzndkprtm@gmail.com>
2026-07-12 21:13:41 +0700

    RAP

---
+++ Setting/Specification/Blind/Add.aspx.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Data.SqlClient
+
+Partial Class Setting_Specification_Blind_Add
+    Inherits Page
+
+    Dim settingClass As New SettingClass
+    Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
+    Dim dataLog As Object() = Nothing
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim pageAccess As Boolean = LoginAccess("Load")
+        If pageAccess = False Then
+            Response.Redirect("~/setting/specification/blind", False)
+            Exit Sub
+        End If
+
+        If Not IsPostBack Then
+            MessageError(False, String.Empty)
+            BindDesign()
+            BindCompany()
+        End If
+    End Sub
+
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs)
+        MessageError(False, String.Empty)
+        Try
+            If txtName.Text = "" Then
+                MessageError(True, "BLIND TYPE IS REQUIRED !")
+                Exit Sub
+            End If
+            If ddlDesign.Text = "" Then
+                MessageError(True, "DESIGN NAME IS REQUIRED !")
+                Exit Sub
+            End If
+            If lbCompany.SelectedValue = "" Then
+                MessageError(True, "COMPANY IS REQUIRED !")
+                Exit Sub
+            End If
+            If msgError.InnerText = "" Then
+                Dim companyDetail As String = String.Empty
+                Dim aliasName As String = txtAlias.Text.Trim()
+
+                If Not lbCompany.SelectedValue = "" Then
+                    companyDetail = String.Join(",", lbCompany.Items.Cast(Of ListItem)().Where(Function(i) i.Selected).Select(Function(i) i.Value))
+                End If
+                If String.IsNullOrEmpty(txtAlias.Text) Then
+                    aliasName = txtName.Text.Trim()
+                End If
+
+                Dim descText As String = txtDescription.Text.Replace(vbCrLf, "").Replace(vbCr, "").Replace(vbLf, "")
+
+                Dim thisId As String = settingClass.CreateId("SELECT TOP 1 Id FROM Blinds ORDER BY Id DESC")
+
+                Using thisConn As New SqlConnection(myConn)
+                    Using thisCmd As SqlCommand = New SqlCommand("INSERT INTO Blinds VALUES (@Id, @DesignId, @CompanyDetailId, @ItemCode, @Name, @Alias, @Description, @Active)", thisConn)
+                        thisCmd.Parameters.AddWithValue("@Id", thisId)
+                        thisCmd.Parameters.AddWithValue("@DesignId", ddlDesign.SelectedValue)
+                        thisCmd.Parameters.AddWithValue("@CompanyDetailId", companyDetail)
+                        thisCmd.Parameters.AddWithValue("@ItemCode", txtItemCode.Text.Trim())
+                        thisCmd.Parameters.AddWithValue("@Name", txtName.Text.Trim())
+                        thisCmd.Parameters.AddWithValue("@Alias", aliasName)
+                        thisCmd.Parameters.AddWithValue("@Description", descText)
+                        thisCmd.Parameters.AddWithValue("@Active", ddlActive.SelectedValue)
+                        thisConn.Open()
+                        thisCmd.ExecuteNonQuery()
+                    End Using
+                End Using
+
+                dataLog = {"Blinds", thisId, Session("LoginId").ToString(), "Blind Type Created"}
+                settingClass.Logs(dataLog)
+
+                Response.Redirect("~/setting/specification/blind", False)
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+            End If
+        End Try
+    End Sub
+
+    Protected Sub btnCancel_Click(sender As Object, e As EventArgs)
+        Response.Redirect("~/setting/specification/blind", False)
+    End Sub
+
+    Protected Sub BindDesign()
+        ddlDesign.Items.Clear()
+        Try
+            ddlDesign.DataSource = settingClass.GetDataTable("SELECT * FROM Designs ORDER BY Name ASC")
+            ddlDesign.DataTextField = "Name"
+            ddlDesign.DataValueField = "Id"
+            ddlDesign.DataBind()
+
+            If ddlDesign.Items.Count > 0 Then
+                ddlDesign.Items.Insert(0, New ListItem("", ""))
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+            End If
+        End Try
+    End Sub
+
+    Protected Sub BindCompany(Optional isEdit As Boolean = False)
+        lbCompany.Items.Clear()
+        Try
+            Dim thisString As String = "SELECT * FROM CompanyDetails WHERE Active=1 ORDER BY Name ASC"
+            If isEdit = True Then
+                thisString = "SELECT * FROM CompanyDetails ORDER BY Name ASC"
+            End If
+            lbCompany.DataSource = settingClass.GetDataTable(thisString)
+            lbCompany.DataTextField = "Name"
+            lbCompany.DataValueField = "Id"
+            lbCompany.DataBind()
+
+            If lbCompany.Items.Count > 0 Then
+                lbCompany.Items.Insert(0, New ListItem("", ""))
+            End If
+        Catch ex As Exception
+            MessageError(True, ex.ToString())
+            If Not Session("RoleName") = "Developer" Then
+                MessageError(True, "PLEASE CONTACT IT SUPPORT AT REZA@BIGBLINDS.CO.ID !")
+            End If
+        End Try
+    End Sub
+
+    Protected Sub MessageError(visible As Boolean, message As String)
+        divError.Visible = visible : msgError.InnerText = message
+    End Sub
+
+    Protected Function LoginAccess(action As String) As Boolean
+        Try
+            Dim roleId As String = Session("RoleId").ToString()
+            Dim levelId As String = Session("LevelId").ToString()
+            Dim accessClass As New AccessClass
+
+            Return accessClass.GetLoginAccess(roleId, levelId, Page.Title, action)
+        Catch ex As Exception
+            Response.Redirect("~/account/login", False)
+            HttpContext.Current.ApplicationInstance.CompleteRequest()
+            Return False
+        End Try
+    End Function
+End Class
