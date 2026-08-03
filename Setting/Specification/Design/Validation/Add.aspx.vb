01885df5d9d0af21b24f18c0748c420295ca1cc9
rzndkprtm <rzndkprtm@gmail.com>
2026-08-03 08:20:53 +0700

    RAP

---
+++ Setting/Specification/Design/Validation/Add.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class Setting_Specification_Design_Validation_Add
+    Inherits System.Web.UI.Page
+
+End Class
---
+++ Setting/Specification/Design/Validation/Condition/Default.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class Setting_Specification_Design_Validation_Condition_Default
+    Inherits System.Web.UI.Page
+
+End Class
