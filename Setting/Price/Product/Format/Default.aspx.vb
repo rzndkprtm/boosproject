f8ce35a37c26c1ddb0734b0871719202f4a8e180
rzndkprtm <rzndkprtm@gmail.com>
2026-08-09 21:43:36 +0700

    RAP

--- Setting/Price/Product/Format/Default.aspx.vb
+++ Setting/Price/Product/Format/Default.aspx.vb
@@ -86,7 +86,7 @@ Partial Class Setting_Price_Product_Format_Default
             gvList.DataBind()
             gvList.Columns(1).Visible = LoginAccess("Visible ID") ' ID
 
-            'btnAdd.Visible = LoginAccess("Add")
+            btnAdd.Visible = LoginAccess("Add")
         Catch ex As Exception
             MessageError(True, ex.ToString())
             If Not Session("RoleName") = "Developer" Then
