e06a41e20f40ee56eae21eea5bfaf32ed03be3d9
rzndkprtm <rzndkprtm@gmail.com>
2026-08-18 14:31:41 +0700

    RAP

--- Order/Method.aspx.vb
+++ Order/Method.aspx.vb
@@ -1557,7 +1557,7 @@ Partial Class Order_Method
 
 
         If blindName = "Complete Set (Single)" OrElse blindName = "Curtain Only" Then
-            If data.companydetailid = "5" OrElse data.companydetailid = "5" Then
+            If data.companydetailid = "5" OrElse data.companydetailid = "9" Then
                 If String.IsNullOrEmpty(data.cutlength) Then Return "FABRIC CUT LENGTH IS REQUIRED !"
                 If Not Integer.TryParse(data.cutlength, cutlength) OrElse cutlength <= 0 Then
                     Return "PLEASE CHECK YOUR FABRIC CUT LENGTH ORDER !"
