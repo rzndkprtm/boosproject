df54e5dd387a621c93a443d3df7d7d4a6f86793f
rzndkprtm <rzndkprtm@gmail.com>
2026-09-10 20:26:07 +0700

    RAP

--- Order/Detail.aspx.vb
+++ Order/Detail.aspx.vb
@@ -4075,7 +4075,7 @@ Partial Class Order_Detail
     Protected Function VisibleCosting() As Boolean
         Dim result As Boolean = False
 
-        If Session("RoleName") = "Developer" OrElse Session("RoleName") = "IT" OrElse Session("RoleName") = "Factory Office" OrElse Session("RoleName") = "Sales" OrElse Session("RoleName") = "Data Entry" OrElse Session("RoleName") = "Account" OrElse Session("RoleName") = "Customer" OrElse Session("RoleName") = "Export" Then result = True
+        If Session("RoleName") = "Developer" OrElse Session("RoleName") = "IT" OrElse Session("RoleName") = "Factory Office" OrElse Session("RoleName") = "Sales" OrElse Session("RoleName") = "Data Entry" OrElse Session("RoleName") = "Account" OrElse Session("RoleName") = "Export" Then result = True
 
         If Session("RoleName") = "Customer" AndAlso Session("PriceAccess") = "Yes" Then result = True
 
