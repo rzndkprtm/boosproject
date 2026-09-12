2f72d525e5fc5bdd8f24f1d403c7d90a65abbf7c
rzndkprtm <rzndkprtm@gmail.com>
2026-09-12 22:05:22 +0700

    RAP

--- Setting/Customer/Multiple.aspx.vb
+++ Setting/Customer/Multiple.aspx.vb
@@ -6,11 +6,11 @@ Partial Class Setting_Customer_Multiple
     Dim myConn As String = ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'Dim pageAccess As Boolean = LoginAccess("Load")
-        'If pageAccess = False Then
-        '    Response.Redirect("~/setting/customer/list", False)
-        '    Exit Sub
-        'End If
+        Dim pageAccess As Boolean = LoginAccess("Load")
+        If pageAccess = False Then
+            Response.Redirect("~/setting/customer/list", False)
+            Exit Sub
+        End If
 
         If Not IsPostBack Then
             MessageError(False, String.Empty)
